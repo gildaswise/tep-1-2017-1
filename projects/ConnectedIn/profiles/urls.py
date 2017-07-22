b7abb9bae8e302156b517c3c45593c136ccda697
Gildaswise <gildasiogx@gmail.com>
2017-07-22 14:43:27 -0300

    Verificação de perfil logado melhorada

--- projects/ConnectedIn/profiles/urls.py
+++ projects/ConnectedIn/profiles/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    url(r'^profile/(?P<pk>\d+)/$', views.profile, name="show_profile"),
+    url(r'^profile/(?P<pk>\d+)/$', views.show_profile, name="show_profile"),
     url(r'^profile/(?P<pk>\d+)/invite$', views.invite, name="invite_profile"),
     url(r'^profile/(?P<invite_pk>\d+)/accept', views.invite_accept, name="invite_accept"),
     url(r'^invites$', views.view_invites, name="invites"),
--- projects/ConnectedIn/profiles/views.py
+++ projects/ConnectedIn/profiles/views.py
@@ -3,33 +3,39 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from .models import *
 
+
 def index(request):
-    current_profile = current_user(request)
+    current_profile = get_current_profile(request)
     profiles = None
-    return render(request, 'index.html', {"current_user": current_profile, "profiles": profiles})
+    if current_profile:
+        profiles = Profile.objects.all()
+    return render(request, 'index.html', {"current_profile": current_profile, "profiles": profiles})
 
 
 @login_required
-def profile(request, pk):
-    current_profile = current_user(request)
+def show_profile(request, pk):
+    current_profile = get_current_profile(request)
     pfl = Profile.objects.get(pk=pk)
     return render(request, 'profile.html', {
-        "current_user": current_profile,
+        "current_profile": current_profile,
         "is_friend": current_profile.is_friend_of(pfl),
         "profile": pfl,
     })
 
 
-@login_required
-def current_user(request):
-    return request.user.profile
+def get_current_profile(request):
+    try:
+        profile = request.user.profile
+        return profile if (isinstance(profile, Profile)) else None
+    except:
+        return None
 
 
 @login_required
 def view_invites(request):
-    current_profile = current_user(request)
+    current_profile = get_current_profile(request)
     return render(request, 'invite.html', {
-        "current_user": current_profile,
+        "current_profile": current_profile,
         "invites": current_profile.invites_made.all(),
         "invited": current_profile.invites_received.all()})
 
@@ -37,10 +43,10 @@ def view_invites(request):
 @login_required
 def invite(request, pk):
     invited_profile = Profile.objects.get(id=pk)
-    current_profile = current_user(request)
+    current_profile = get_current_profile(request)
     current_profile.invite(invited_profile)
     return render(request, 'invite.html', {
-        "current_user": current_profile,
+        "current_profile": current_profile,
         "invites": current_profile.invites_made.all(),
         "invited": current_profile.invites_received.all()
     })
@@ -55,8 +61,8 @@ def invite_accept(request, invite_pk):
 
 @login_required
 def view_friends(request):
-    current_profile = current_user(request)
+    current_profile = get_current_profile(request)
     return render(request, 'friends.html', {
-        "current_user": current_profile,
+        "current_profile": current_profile,
         "friends": current_profile.friends.all()
     })
