a2e8a46a4e18a94fa86f0eee65582c40845e6784
Gildaswise <gildasiogx@gmail.com>
2017-08-18 17:18:21 -0300

    Feita a atividade de validações na API de partidas

--- projects/GamesAPI/games/serializers.py
+++ projects/GamesAPI/games/serializers.py
@@ -1,2 +1,28 @@
 from rest_framework import serializers
 from .models import Game
+
+
+class GameSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Game
+        fields = ("id", "name", "release_date", "game_category", "played",)
+
+    def is_empty(self, value):
+        if value is None or value == "":
+            raise serializers.ValidationError("Value shouldn't be empty!")
+        return value
+
+    def is_registered(self, value):
+        if value in list(map(lambda it: it.name, Game.objects.filter(name=value).exclude(id=self.initial_data['id']))):
+            raise serializers.ValidationError("This game is already registered!")
+
+    def validate_name(self, name):
+        self.is_registered(name)
+        return self.is_empty(value=name)
+
+    def validate_release_date(self, release_date):
+        return self.is_empty(value=release_date)
+
+    def validate_game_category(self, game_category):
+        return self.is_empty(value=game_category)
--- projects/GamesAPI/games/views.py
+++ projects/GamesAPI/games/views.py
@@ -5,15 +5,48 @@ Author: Gaston C. Hillar - Twitter.com/gastonhillar
 Publisher: Packt Publishing Ltd. - http://www.packtpub.com
 """
 
+from datetime import datetime
+from django.utils import timezone
+from rest_framework.generics import get_object_or_404
 from rest_framework.parsers import JSONParser
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import GameSerializer
 from .models import Game
 
+
+@api_view(['GET', 'POST'])
 def game_list(request):
-    return
+    if request.method == 'GET':
+        games = Game.objects.all()
+        games_serializer = GameSerializer(games, many=True)
+        return Response(games_serializer.data)
+    elif request.method == 'POST':
+        game_serializer = GameSerializer(data=request.data)
+        if game_serializer.is_valid():
+            game_serializer.save()
+            return Response(game_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def game_detail(request, pk):
-    return
+    game = get_object_or_404(Game, id=pk)
 
+    if request.method == 'GET':
+        game_serializer = GameSerializer(game)
+        return Response(game_serializer.data)
+    elif request.method == 'PUT':
+        game_serializer = GameSerializer(game, data=request.data)
+        if game_serializer.is_valid():
+            game_serializer.save()
+            return Response(game_serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        today = timezone.make_aware(datetime.now(), timezone.get_current_timezone())
+        if today > game.release_date:
+            game.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response({"release_date": "This game hasn't launched yet!"}, status=status.HTTP_403_FORBIDDEN)
