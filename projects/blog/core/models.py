67f3178f343ee246c99cb98a38d83ce5debc4abe
Gildaswise <gildasiogx@gmail.com>
2017-08-28 15:20:43 -0300

    Adicionado conversor do dump de JSON e a aplicação blog para atividade de APIs

--- projects/blog/core/models.py
+++ projects/blog/core/models.py
@@ -5,6 +5,9 @@ class Geo(models.Model):
     lat = models.FloatField()
     lng = models.FloatField()
 
+    def __str__(self):
+        return "lat: %s\nlng:%s" % (self.lat, self.lng)
+
 
 class Address(models.Model):
     street = models.CharField(max_length=128)
@@ -13,6 +16,9 @@ class Address(models.Model):
     zipcode = models.CharField(max_length=10)
     geo = models.ForeignKey(Geo)
 
+    def __str__(self):
+        return "%s, %s, %s, %s at %s" % (self.street, self.suite, self.city, self.zipcode, self.geo)
+
 
 class User(models.Model):
     username = models.CharField(max_length=128)
@@ -20,11 +26,17 @@ class User(models.Model):
     email = models.EmailField()
     address = models.ForeignKey(Address)
 
+    def __str__(self):
+        return "@%s - %s - %s" % (self.username, self.name, self.email)
+
 
 class Post(models.Model):
     title = models.CharField(max_length=128)
     body = models.TextField()
     user = models.ForeignKey(User, related_name="posts")
+
+    def __str__(self):
+        return "Post from @%s - %s" % (self.user.username, self.title)
 
 
 class Comment(models.Model):
@@ -32,3 +44,6 @@ class Comment(models.Model):
     email = models.EmailField()
     body = models.TextField()
     post = models.ForeignKey(Post, related_name="comments")
+
+    def __str__(self):
+        return "Comment from %s" % (self.name)
